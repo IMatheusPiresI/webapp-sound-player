c76c016cd495255e0575a8eb3a0ece7ef7f2f964
Matheus Pires <matheuspdsousa@gmail.com>
2023-05-23 00:08:24 -0300

    feat: create and adjust form add music

--- webapp-app-sound-player/src/app/(auth)/musics/add/_components/FormAddMusic/index.tsx
+++ webapp-app-sound-player/src/app/(auth)/musics/add/_components/FormAddMusic/index.tsx
@@ -1,12 +1,14 @@
 'use client';
 
 import { IState } from '@/app/sign/_components/FormSignIn/types';
+import { Button } from '@/components/Button';
 import { InputForm } from '@/components/Form/InputForm';
 import React, { useState } from 'react';
 
 export const FormAddMusic = () => {
   const [musicName, setMusicName] = useState<string>('');
   const [musicCreator, setMusicCreator] = useState<string>('');
+  const [musicGenre, setMusicGenre] = useState<string>('');
 
   const handleChangeValueInput =
     (setState: IState) => (event: React.FormEvent<HTMLInputElement>) => {
@@ -14,8 +16,8 @@ export const FormAddMusic = () => {
     };
 
   return (
-    <form className="flex-1 w-full flex flex-col items-center mt-10">
-      <div className="relative w-full max-w-[250px] h-[325px] border border-white">
+    <form className="flex-1 w-full flex flex-col items-center mt-10 pb-10">
+      <div className="relative w-full max-w-[250px] h-[325px] border border-white ">
         <div className="absolute items-center flex flex-col justify-center w-full h-full">
           <p className="text-white/[0.4] text-center font-medium text-xl">
             MUSIC
@@ -26,15 +28,81 @@ export const FormAddMusic = () => {
       </div>
 
       <div className="mt-12">
-        <InputForm
-          label="Nome da Música"
-          id="music_name"
-          type="text"
-          value={musicName}
+        <div className="h-20 border border-white mb-6 relative">
+          <div className="absolute h-full w-full flex items-center justify-center">
+            <p className="text-white/[0.4] text-center font-medium text-xl">
+              MUSIC FILE .MP3
+            </p>
+          </div>
+        </div>
+        <select
+          name="genre_music"
+          id="genre_music"
+          defaultValue="Gênero da Música"
           required
-          minLength={3}
-          onChange={handleChangeValueInput(setMusicName)}
-        />
+          className="w-full p-3 border border-white rounded-lg bg-transparent text-white outline-none"
+        >
+          <option
+            className="text-white bg-neutral-800 border border-white hover:bg-neutral-500"
+            value="Gênero da Música"
+          >
+            Gênero da Música
+          </option>
+          <option
+            className="text-white bg-neutral-800 border border-white hover:bg-neutral-500"
+            value="R.A.P"
+          >
+            R.A.P
+          </option>
+          <option
+            className="text-white bg-neutral-800 border border-white hover:bg-neutral-500"
+            value="Funk"
+          >
+            Funk
+          </option>
+          <option
+            className="text-white bg-neutral-800 border border-white hover:bg-neutral-500"
+            value="Dance"
+          >
+            Dance
+          </option>
+          <option
+            className="text-white bg-neutral-800 border border-white hover:bg-neutral-500"
+            value="Pagode"
+          >
+            Pagode
+          </option>
+          <option
+            className="text-white bg-neutral-800 border border-white hover:bg-neutral-500"
+            value="Rock"
+          >
+            Rock
+          </option>
+          <option
+            className="text-white bg-neutral-800 border border-white hover:bg-neutral-500"
+            value="Eletronic"
+          >
+            Eletronic
+          </option>
+          <option
+            className="text-white bg-neutral-800 border border-white"
+            value="P.O.P"
+          >
+            P.O.P
+          </option>
+        </select>
+
+        <div className="mt-6">
+          <InputForm
+            label="Nome da Música"
+            id="music_name"
+            type="text"
+            value={musicName}
+            required
+            minLength={3}
+            onChange={handleChangeValueInput(setMusicName)}
+          />
+        </div>
 
         <div className="mt-6">
           <InputForm
@@ -48,54 +116,7 @@ export const FormAddMusic = () => {
           />
         </div>
         <div className="mt-6 w-full">
-          <select
-            name=""
-            id=""
-            className="w-full p-3 border border-white rounded-lg bg-transparent text-white"
-          >
-            <option
-              className="text-white bg-neutral-800 border border-white hover:bg-neutral-500"
-              value="R.A.P"
-            >
-              R.A.P
-            </option>
-            <option
-              className="text-white bg-neutral-800 border border-white hover:bg-neutral-500"
-              value="Funk"
-            >
-              Funk
-            </option>
-            <option
-              className="text-white bg-neutral-800 border border-white hover:bg-neutral-500"
-              value="Dance"
-            >
-              Dance
-            </option>
-            <option
-              className="text-white bg-neutral-800 border border-white hover:bg-neutral-500"
-              value="Pagode"
-            >
-              Pagode
-            </option>
-            <option
-              className="text-white bg-neutral-800 border border-white hover:bg-neutral-500"
-              value="Rock"
-            >
-              Rock
-            </option>
-            <option
-              className="text-white bg-neutral-800 border border-white hover:bg-neutral-500"
-              value="Eletronic"
-            >
-              Eletronic
-            </option>
-            <option
-              className="text-white bg-neutral-800 border border-white"
-              value="P.O.P"
-            >
-              P.O.P
-            </option>
-          </select>
+          <Button title="Add Music" />
         </div>
       </div>
     </form>
